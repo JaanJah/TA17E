984e8cbaaac1d1fff391a940c199ca0db4b44fa0
Priit <priitmat@gmail.com>
2017-11-02 08:42:46 +0200

    added class mammal

---
+++ Objektid/Objektid/Mammal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektid
+{
+    class Mammal
+    {
+        public Mammal()
+        {
+            NumberOfEyes = 2;
+            IsCarnivore = true;
+            Breathe();
+        }
+
+        public int NumberOfEyes { get; set; }
+        public bool IsCarnivore { get; set; }
+        public void Breathe()
+        {
+            Console.WriteLine("I am breathing!");
+        }
+    }
+}
